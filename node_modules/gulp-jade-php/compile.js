5cc896333f0fca6b2a1747f7e3557732801d24da
Luke Hatfield <luke@lukehatfield.com>
2016-10-05 00:59:40 -0400

    dont track node modules

--- node_modules/gulp-jade-php/compile.js
+++
@@ -1,6 +0,0 @@
-var jade = require('jade');
-function compile(contents, opts){
-  return jade.compile(contents, opts)(opts.locals || opts.data);
-}
-
-module.exports = compile;
--- node_modules/gulp-jade-php/ext.js
+++
@@ -1,6 +0,0 @@
-var ext = require('gulp-util').replaceExtension;
-function extension(filepath, opts) {
-  return ext(filepath, opts.extension || '.php');
-}
-
-module.exports = extension;
