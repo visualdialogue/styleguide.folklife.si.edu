e6c07f36e36835f75d260f6d88ea8b369c319920
Luke Hatfield <luke@lukehatfield.com>
2016-10-13 16:09:05 -0400

    never cache css

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -6,8 +6,7 @@
 <title>Smithsonian Center for Folklife and Cultural Heritage | <?php echo $page->title()->html() ?></title><script src="https://use.typekit.net/roi1emx.js"></script>
 <script>try{Typekit.load({ async: true });}catch(e){}</script>
 <!-- “Entypo pictograms by Daniel Bruce — www.entypo.com” -->
-<link href="assets/css/bootstrap.css" rel="stylesheet">
-<link href="assets/css/folklife.css?v=10.13" rel="stylesheet">
+<link href="assets/css/bootstrap.css" rel="stylesheet"><link href="assets/css/folklife.css?v=<?= time() ?>" rel="stylesheet">
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
