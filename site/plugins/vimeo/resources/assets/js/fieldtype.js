279a7afd4f629f2ccb2b802bd553b2a4a2a49de8
Luke Hatfield <luke@lukehatfield.com>
2016-10-20 11:49:50 -0400

    remodal outside

---
+++ site/plugins/vimeo/resources/assets/js/fieldtype.js
@@ -0,0 +1,25 @@
+Vue.component('vimeo-fieldtype', {
+
+    template: '<div>Vimeo</div>',
+
+    props: ['data', 'config', 'name'],
+
+    data: function() {
+        return {
+            //
+        };
+    },
+
+    computed: {
+        //
+    },
+
+    methods: {
+        //
+    },
+
+    ready: function() {
+        //
+    }
+
+});
---
+++ site/plugins/vimeo/vimeo.php
@@ -0,0 +1,12 @@
+<?php 
+
+function hello($video_id) {
+	
+    require_once(__DIR__ . '/../../../../config/folklife.si.edu/vimeo-config.php'); // get api creds
+    require_once(__DIR__ . '/resources/vimeo.php-1.2.3/src/Vimeo/Vimeo.php'); // vimeo api php library
+
+
+
+	echo $category;
+
+}
