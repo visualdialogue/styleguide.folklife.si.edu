b45ac365a3f681e9bd0d0180a1dc0aa83ca9ac74
Luke Hatfield <luke@lukehatfield.com>
2016-10-21 11:21:04 -0400

    new critical

--- gulpfile.js
+++ gulpfile.js
@@ -111,7 +111,7 @@ gulp.task('critical', function () {
       dest: './assets/css/critical.css',
       extract: false,
       ignore: [/^\.remodal/],
-      include: [/^\.slick/,/^\.col-/]
+      include: [/^\.slick/,/^\.col-/,/^\.section--first/]
     });
     // critical.generateInline({
     //   base: 'test/',
