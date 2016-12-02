4b377efee2e25e702953b50a60471b3099406f6e
Luke Hatfield <luke@lukehatfield.com>
2016-12-01 21:47:35 -0500

    common

---
+++ site/common/fonts.php
@@ -0,0 +1,10 @@
+<script src="https://use.typekit.net/roi1emx.js"></script><script>//try{Typekit.load({ async: true });}catch(e){}</script>
+<script>try{Typekit.load();}catch(e){}</script>
+<!-- “Entypo pictograms by Daniel Bruce — www.entypo.com” -->
+<!-- Font Awesome icons by Dave Gandy --><style>.wf-loading .tagline,
+.wf-loading .site-toggle__link,
+.wf-loading .title,
+.wf-loading .section__title {
+	/* Hide the blog title and post titles while web fonts are loading */
+	visibility: hidden;
+}</style>
