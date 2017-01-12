6bb3b20cbc598d110f37dc9ddbadec856204a920
Luke Hatfield <luke@lukehatfield.com>
2017-01-11 20:17:39 -0500

    mobile buttons

--- site/snippets/folkways-suggests.php
+++ site/snippets/folkways-suggests.php
@@ -2,25 +2,35 @@
 <div class="row">
   <div class="album-info col-sm-12">
     <h3 class="sidebar-section-title">Folkways Suggests</h3>
-    <div class="album-sidebar-tab">ALBUMS 
-      <div aria-hidden="true" class="icon-down-open-mini"></div>
+    <div class="button">
+      <div class="button-inner album-sidebar-tab">ALBUMS 
+        <div aria-hidden="true" class="icon-down-open-mini"></div>
+      </div>
     </div>
     <ul class="row suggested-albums">
       <?php foreach($pages->find('explore')->children()->find('sounds')->children()->filterBy('tags', '!=', 'radio')->shuffle()->limit(6) as $album): ?>
       <?php snippet('card-horizontal', array('card' => $album, 'card_classes' => 'col-xs-6 col-sm-12 suggested-album sidebar-card top-border-card', 'image_col' => 'col-sm-6', 'details_col' => 'col-sm-6', 'artist' => true )); ?>
       <?php endforeach; ?>
     </ul>
-    <div class="album-sidebar-tab">VIDEOS 
-      <div aria-hidden="true" class="icon-right-open-mini"></div>
+    <div class="button">
+      <div class="button-inner album-sidebar-tab">VIDEOS 
+        <div aria-hidden="true" class="icon-right-open-mini"></div>
+      </div>
     </div>
-    <div class="album-sidebar-tab">PLAYLISTS
-      <div aria-hidden="true" class="icon-right-open-mini"></div>
+    <div class="button">
+      <div class="button-inner album-sidebar-tab">PLAYLISTS
+        <div aria-hidden="true" class="icon-right-open-mini"></div>
+      </div>
     </div>
-    <div class="album-sidebar-tab">TOOLS FOR TEACHING
-      <div aria-hidden="true" class="icon-right-open-mini"></div>
+    <div class="button">
+      <div class="button-inner album-sidebar-tab">TOOLS FOR TEACHING
+        <div aria-hidden="true" class="icon-right-open-mini"></div>
+      </div>
     </div>
-    <div class="album-sidebar-tab">ARTICLES
-      <div aria-hidden="true" class="icon-right-open-mini"></div>
+    <div class="button">
+      <div class="button-inner album-sidebar-tab">ARTICLES
+        <div aria-hidden="true" class="icon-right-open-mini"></div>
+      </div>
     </div>
   </div>
 </div>
