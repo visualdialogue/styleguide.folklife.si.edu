7638b86772f55ddb9fd85e1d2ab1803252f5f4a5
Luke Hatfield <luke@lukehatfield.com>
2017-03-23 16:23:19 -0400

    figcaption links and blog sidebar bottom padding

--- site/common/snippets/blog-most-popular.php
+++ site/common/snippets/blog-most-popular.php
@@ -1,14 +1,1 @@
-
-<div class="blog-most-popular sidebar-section">
-  <h3 class="sidebar-section-title">Most Popular</h3>
-  <ul>
-    <?php $n=0; ?>
-    <?php foreach($pages->find('blog')->children()->visible()->filterBy('tags', 'popular', ',')->limit(5) as $entry): $n++; ?>
-    <li class="sidebar-item numbered-sidebar-item sidebar-highlight">
-      <div class="blog-sidebar-number">
-        <?php echo $n; ?></div><a href="<?= $entry->url() ?>" class="title">
-        <?php echo $entry->title()->excerpt('8', 'words'); ?></a>
-    </li>
-    <?php endforeach; ?>
-  </ul>
-</div>
+<div class="blog-most-popular sidebar-section"><h3 class="sidebar-section-title">Most Popular</h3><ol><?php $n=0; ?><?php foreach($pages->find('blog')->children()->visible()->filterBy('tags', 'popular', ',')->limit(5) as $entry): $n++; ?><li class="sidebar-item numbered-sidebar-item sidebar-highlight"><a href="<?= $entry->url() ?>" class="title"><?php echo $entry->title()->excerpt('8', 'words'); ?></a></li><?php endforeach; ?></ol></div>
